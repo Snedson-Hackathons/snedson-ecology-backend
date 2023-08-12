d625ebfeb3b9a205758917d72bd79a0380d5ba54
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-08-12 22:05:37 +0500

    created abstract feature handler

---
+++ snedson-ecology-backend.core/Features/AbstractFeatureHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using snedson_ecology_backend.core.Interfaces;
+
+namespace snedson_ecology_backend.core.Features
+{
+    internal abstract class AbstractFeatureHandler
+    {
+        protected readonly IEcologyContext db;
+        public AbstractFeatureHandler(IEcologyContext context)
+        {
+            db = context;
+        }
+    }
+
+    internal abstract class AbstractFeatureHandlerWithMediatr : AbstractFeatureHandler
+    {
+        protected readonly IMediator _mediator;
+
+        public AbstractFeatureHandlerWithMediatr
+            (IEcologyContext context, IMediator mediator) : base(context)
+        {
+            _mediator = mediator;
+        }
+    }
+}
