7218d84bb6b96ca7c6a3a0d1c279a0b59e071420
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-08-12 18:33:12 +0500

    Добавьте файлы проекта.

---
+++ snedson-ecology-backend/Controllers/EventController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace snedson_ecology_backend.Controllers
+{
+    [Route("/api")]
+    [ApiController]
+    public class EventController : Controller
+    {
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Events(int take, int offset)
+        {
+            //MainPageRM toReturn = await _mainPageFiller.FillModel(StudentId);
+            return Ok("Hej");
+        }
+
+        [HttpGet("event")]
+        public async Task<IActionResult> GetEvent(Guid id)
+        {
+            //MainPageRM toReturn = await _mainPageFiller.FillModel(StudentId);
+            return Ok("Hej");
+        }
+
+        [HttpPost("event")]
+        public async Task<IActionResult> CreateEvent(Guid id)
+        {
+            //MainPageRM toReturn = await _mainPageFiller.FillModel(StudentId);
+            return Ok("Hej");
+        }
+
+    }
+}
