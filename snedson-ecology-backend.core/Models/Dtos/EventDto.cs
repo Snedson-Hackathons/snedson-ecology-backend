f7d4004a602866617a7d9d002c38ff4e84d7ac28
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-08-12 22:03:39 +0500

    created events related models

---
+++ snedson-ecology-backend.core/Models/Dtos/EventDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace snedson_ecology_backend.core.Models.Dtos
+{
+    public record EventDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Guid Author { get; set; }
+        public string Description { get; set; }
+        public long ScheduledDate { get; set; }
+        public string LocationLat { get; set; }
+        public string LocationLon { get; set; }
+        public string ExpectedResult { get; set; }
+        public string ActualResult { get; set; }
+        public EventNeedDto[] Needs { get; set; }
+    }
+}
